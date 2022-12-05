f727e532b55a2d8b6d8ba66a0fc986d556f7f110
Alexander Lind <alex.anonym@gmail.com>
2022-12-05 12:25:02 +0100

    finished exercise

--- src/router.js
+++ src/router.js
@@ -14,11 +14,24 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
+    const userInput = req.body;
 
+    if (userInput.username === mockUser.username && userInput.password === mockUser.password) {
+        const token = jwt.sign({username : userInput.username}, process.env.JWT_SECRET)
+        return res.json({token});
+    }
+    return res.json({error: 'Invalid username or password'});
 });
 
 router.get('/profile', (req, res) => {
-  
+  const token = req.get('authorization');
+    try {
+        jwt.verify(token, process.env.JWT_SECRET)
+        return res.json(mockUser.profile) 
+    } catch (error) {
+        return res.json({error: 'wrong token or invalid secret phrase'})
+    }
+
 });
 
 
